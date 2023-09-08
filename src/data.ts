c35f351832091a565dbd2054ae2105f4cd197c17
Samson Cain <samsonc99@gmail.com>
2023-09-08 09:15:32 -0500

    chore: add 4th level

--- src/data.ts
+++ src/data.ts
@@ -1,22 +1,33 @@
 export const NAVIGATION_DATA = [
   {
     level: 1,
-    title: "This is L1",
+    title: "L1-1",
     children: [
       {
         level: 2,
-        title: "This is L2",
+        title: "L2-1",
         children: [
           {
             level: 3,
-            title: "This is L3",
-            children: [],
+            title: "L3-1",
+            children: [
+              {
+                level: 4,
+                title: "L4-1",
+                children: [],
+              },
+              {
+                level: 4,
+                title: "L4-1",
+                children: [],
+              },
+            ],
           },
         ],
       },
       {
         level: 2,
-        title: "This is L2",
+        title: "L2-2",
         children: [],
       },
     ],
